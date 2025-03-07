16a3a73fc556f6ac756c749d763a9ce31c264d15
bond_anton <bond.anton@gmail.com>
2025-03-07 13:04:55 +0300

    Dependency version corrected.

--- examples/simple_sensor.py
+++ examples/simple_sensor.py
@@ -1,9 +1,10 @@
+"""Simple Linear Sensor example."""
 from nts.hardware.analog_sensor import LinearSensor
 
 
 if __name__ == "__main__":
     my_sensor = LinearSensor("LinearSensor", gain=2, offset=0)
-    voltage = 3.3
-    value = my_sensor.convert_voltage(voltage)
+    VOLTAGE = 3.3
+    value = my_sensor.convert_voltage(VOLTAGE)
 
-    print(f"Sensor reading {voltage} V. Sensor data: {value}.")
+    print(f"Sensor reading {VOLTAGE} V. Sensor data: {value}.")
